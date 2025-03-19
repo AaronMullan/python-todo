8bdbc80036d163b997f1b459a290f6abec8dd56f
AaronMullan <aaroncmullan@gmail.com>
2025-03-19 09:49:27 -0700

    specific urls allowed

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["http://localhost:3000"],  # Your frontend URL
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
